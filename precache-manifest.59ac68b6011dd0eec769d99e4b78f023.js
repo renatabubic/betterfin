2d35c7bf39cae12b1cc62ef81e2a2d3f6c96c2e2
Renata <renata.c.bubic@gmail.com>
2020-06-08 09:08:59 -0400

    Updates

---
+++ precache-manifest.59ac68b6011dd0eec769d99e4b78f023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d73fbc52cd9e2f9f5deb9db842cc8f7",
+    "url": "/betterfin/index.html"
+  },
+  {
+    "revision": "c75117ff7971ca3e9908",
+    "url": "/betterfin/static/css/main.101d109e.chunk.css"
+  },
+  {
+    "revision": "46a1cf2e1d26b4c4a643",
+    "url": "/betterfin/static/js/2.63abe10d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/betterfin/static/js/2.63abe10d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75117ff7971ca3e9908",
+    "url": "/betterfin/static/js/main.a8a30838.chunk.js"
+  },
+  {
+    "revision": "f7c18f04598e8fb49422",
+    "url": "/betterfin/static/js/runtime-main.c1125ecf.js"
+  },
+  {
+    "revision": "cb35d28104680bb8e7aca6512c253fa5",
+    "url": "/betterfin/static/media/logo.cb35d281.svg"
+  }
+]);
